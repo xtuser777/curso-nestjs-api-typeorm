9385df2836942cbbc0c3e148ec56a6e1675ec7c0
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-27 10:11:11 -0300

    typeorm migrations config

---
+++ typeorm/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_SCHEMA,
+  migrations: [`${__dirname}/migrations/**/*.ts`],
+});
+
+export default dataSource;
